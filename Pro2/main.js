48f1a515258d29eafc977c2aff59dd4e03723be5
Eyobt4 <eyobtesfaye838@gmail.com>
2024-02-13 12:50:40 -0800

    almost_done

--- Pro2/main.js
+++ Pro2/main.js
@@ -1,7 +1,7 @@
 const display = document.getElementById('display');
 
 function appendToDisplay(input){
-    display.value = display.value + input;
+    display.value += input;
 }
 function clearDisplay(){
     display.value = '';
